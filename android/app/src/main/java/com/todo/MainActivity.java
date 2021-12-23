220041fd88e56de2b9c0b0e20dd4ecebe53767de
shanmukeshwar03 <shanmukeshwar03@gmail.com>
2021-12-24 00:45:57 +0530

    Added Login , Register and OTP Screens

--- android/app/src/main/java/com/todo/MainActivity.java
+++ android/app/src/main/java/com/todo/MainActivity.java
@@ -1,9 +1,17 @@
 package com.shanmukeshwar.todo;
 
+import android.os.Bundle;
+import com.zoontek.rnbootsplash.RNBootSplash;
 import com.facebook.react.ReactActivity;
 
 public class MainActivity extends ReactActivity {
-  
+    
+  @Override
+    protected void onCreate(Bundle savedInstanceState) {
+    RNBootSplash.init(MainActivity.this);
+    super.onCreate(null);
+  }
+
   @Override
   protected String getMainComponentName() {
     return "todo";
